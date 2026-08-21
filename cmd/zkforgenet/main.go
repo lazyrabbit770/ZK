f0f34c6bae8fabca74a7678cfb0c1521294ec61b
Ercílio <lazyrabbit7700324@ccdot.email>
2026-08-22 03:35:26 +0700

    Add main.go
    Co-authored-by: nancodero <nancodero@proton.me>

---
+++ cmd/zkforgenet/main.go
@@ -0,0 +1,23 @@
+// cmd/zkforgenet/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "zkforgenet/internal/zkforgenet"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := zkforgenet.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
